007b979fdfb029159f40fc82339d4d97dd7d775d
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-11-06 14:30:45 -0800

    Start signup view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
+
+from dnd_character_app import Character
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                context = {
+                    'error': 'Username is already taken.',
+                }
+                return render(request, 'landing.html', context)
